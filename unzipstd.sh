17f299ebb9376efebc35e3e7cc3338d02396fd6f
GernotMaier <gernot.maier@desy.de>
2021-01-07 10:19:38 +0100

    unzip to queue

--- unzipstd.sh
+++ unzipstd.sh
@@ -11,7 +11,9 @@ ODIR="/lustre/fs19/group/cta/VERITAS/simulations/V6_FLWO/CARE_June1702_ATM61/"
 # log files are written here
 LDIR="/lustre/fs23/group/cta/tempVTS/CARE_April2020_ATM61/"
 # bz2 files are located here
-LL=`find ./ -name "gamma*.bz2"`
+DDIR="/lustre/fs23/group/cta/tempVTS/CARE_April2020_ATM61/"
+# list of files to be converted
+LL=`find $DDIR -name "gamma*.bz2"`
 
 
 for L in $LL
@@ -26,8 +28,8 @@ do
    fi
 
    echo "Submitting"
+   echo "   $DDIR"
+   echo "   $O"
+   echo "   $ODIR"
    qsub -l h_rt=31:30:00 -o $LDIR -e $LDIR -l tmpdir_size=250G qsub_unzipstd.sh $DDIR $O $ODIR
-
-   exit
-
 done
